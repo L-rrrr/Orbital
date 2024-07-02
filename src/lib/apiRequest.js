13e43c957ec7cf9396c1b3d7312f0b16517f3c4c
L-rrrr <lruniapp1020@gmail.com>
2024-07-02 22:54:47 +0800

    Update apiRequest.js

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+
 const apiRequest = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your actual API base URL
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -20,4 +22,27 @@ apiRequest.interceptors.response.use((response) => {
 });
 
 export default apiRequest;
+
+// import axios from 'axios';
+
+// const apiRequest = axios.create({
+//   baseURL: 'http://localhost:5000/api', // Your actual API base URL
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// apiRequest.interceptors.request.use((config) => {
+//   return config;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
+
+// apiRequest.interceptors.response.use((response) => {
+//   return response;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
+
+// export default apiRequest;
 
